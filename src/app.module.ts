2ee82b4434e25eb62d6d59ec7e57935c6b142e8d
Guilherme Negreiros <negreiros.4k@gmail.com>
2025-10-21 20:49:09 -0300

    feat: setup Prisma + PostgreSQL local

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthController } from './health/health.controller';
+import { HealthModule } from './health/health.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, HealthController],
-  providers: [AppService],
+  imports: [PrismaModule, HealthModule],
 })
 export class AppModule {}
--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { HealthService } from './health.service';
 
 @Controller('health')
 export class HealthController {
+  constructor(private readonly healthService: HealthService) {}
+
   @Get()
-  getStatus() {
-    return { status: 'ok', message: 'API online' };
+  getAppStatus() {
+    return this.healthService.appStatus();
+  }
+
+  @Get('db')
+  getDbStatus() {
+    return this.healthService.dbStatus();
   }
 }
