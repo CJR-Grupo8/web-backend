b49d011b073b087dccbe7c391e250a5c3aface95
vinicius.A <vinicius.araruna18@gmail.com>
2025-11-11 22:46:29 -0300

    chore: atualiza módulo de produtos e registra dependencias necessárias

--- src/produtos/produtos.module.ts
+++ src/produtos/produtos.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProdutosService } from './produtos.service';
 import { ProdutosController } from './produtos.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { IslojaOwnerGuard } from 'src/auth/guards/is-loja-owner.guard';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ProdutosController],
-  providers: [ProdutosService],
+  providers: [ProdutosService, IslojaOwnerGuard],
   exports: [ProdutosService],
 })
 export class ProdutosModule {}
