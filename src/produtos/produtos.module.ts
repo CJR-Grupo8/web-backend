bd24b824446bf75fef4d18718b5c8b628836f253
vinicius.A <vinicius.araruna18@gmail.com>
2025-11-11 23:42:35 -0300

    fix: adiciona os imports corretos e ajusta os providers

--- src/produtos/produtos.module.ts
+++ src/produtos/produtos.module.ts
@@ -3,11 +3,18 @@ import { ProdutosService } from './produtos.service';
 import { ProdutosController } from './produtos.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { IslojaOwnerGuard } from 'src/auth/guards/is-loja-owner.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { AuthGuard } from 'src/auth/guards/auth-guard';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({}),
+    ConfigModule,
+  ],
   controllers: [ProdutosController],
-  providers: [ProdutosService, IslojaOwnerGuard],
+  providers: [ProdutosService, AuthGuard, IslojaOwnerGuard],
   exports: [ProdutosService],
 })
 export class ProdutosModule {}
