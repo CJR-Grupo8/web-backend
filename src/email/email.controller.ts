24f088e497bf8e5b75dbb08f11c93e7ca664169b
vinicius.A <vinicius.araruna18@gmail.com>
2025-11-14 10:01:52 -0300

    feat: configuração das rotas finais

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,7 +1,19 @@
 import { Controller,Post, Body } from "@nestjs/common";
 import { EmailService } from "./email.service";
+import { forgotPasswordDto } from "./dto/forgot-password.dto";
+import { resetPasswordDto } from "./dto/reset-password.dto";
 
 @Controller('email')
 export class EmailController{
     constructor(private readonly emailService: EmailService){}
+
+    @Post('forgot-password')
+    forgotPassword(@Body() dto: forgotPasswordDto){
+        return this.emailService.forgotPassword(dto.email);
+    }
+
+    @Post('reset-password')
+    resetPasswrod(@Body() dto: resetPasswordDto){
+        return this.emailService.resetPassword(dto.token, dto.newPassword);
+    }
 }
